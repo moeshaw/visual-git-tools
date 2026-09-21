8c80bb966e04a48caea8b7f334a021acd73e94d8
moeshaw <moeshaw@umich.edu>
2026-09-21 18:32:45 -0500

    created checkout file that calculate subtotal with tax

---
+++ checkout.py
@@ -0,0 +1,12 @@
+prices = [10.99, 5.50, 20.00, 3.25]
+
+# Calculate total directly
+subtotal = sum(prices)
+
+# Optional Add 8% tax to get the final total
+tax = subtotal  0.08
+total = subtotal + tax
+
+print(fSubtotal ${subtotal.2f})
+print(fTax (8%) ${tax.2f})
+print(fTotal    ${total.2f})
